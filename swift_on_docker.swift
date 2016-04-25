cffc757759fb5a68b1e7520c2107d58708bc322c
Yan Li <yan.li@ef.com>
2016-04-25 19:03:29 +0800

    Fix spaces

--- swift_on_docker.swift
+++ swift_on_docker.swift
@@ -1,73 +1,73 @@
 let slides = Slides(pages: [
 
-	// Title	
-	
-	Cover(title: "Swift on Docker", bulletPoints: ["v0.2.1"]),
+  // Title	
 
-	// Section 1
+  Cover(title: "Swift on Docker", bulletPoints: ["v0.2.1"]),
 
-	Page(title: "What is Docker", bulletPoints: [
-		"Let you separate your applications from your infrastructure,",
-   	"and treat your infrastructure like a managed application,",
-   	"...without a hypervisor.",
-	]),
+  // Section 1
 
-	Page(title: "What can we use Docker for", bulletPoints: [
-		"Run the latest development snapshot",
-   	"Build and test swift.org projects",
-	]),
+  Page(title: "What is Docker", bulletPoints: [
+    "Let you separate your applications from your infrastructure,",
+    "and treat your infrastructure like a managed application,",
+    "...without a hypervisor.",
+  ]),
 
-	Page(title: "Build a Docker image", bulletPoints: [
-		"Docker image: your infrastructure, considered immutable",
-   	"Built by reading the instructions from a Dockerfile",
-	]),
+  Page(title: "What can we use Docker for", bulletPoints: [
+    "Run the latest development snapshot",
+    "Build and test swift.org projects",
+  ]),
 
-	Cover(title: "Example: eyeplum/swift-dev", bulletPoints: []),
+  Page(title: "Build a Docker image", bulletPoints: [
+    "Docker image: your infrastructure, considered immutable",
+    "Built by reading the instructions from a Dockerfile",
+  ]),
 
-	// Section 2
+  Cover(title: "Example: eyeplum/swift-dev", bulletPoints: []),
 
-	Page(title: "Run the image in a container", bulletPoints: [
-		"Docker container: running image + runtime user data",
-   	"Containers are considered short running and easily re-creatable",
-   	"Don’t put important user data in containers",
-   	"    Copy them as resources",
-   	"    Load them as data volumes",
-   	"See: Best practices for writing Dockerfiles",
-	]),
+  // Section 2
 
-	Cover(title: "Example: drawRect.swift", bulletPoints: []),
+  Page(title: "Run the image in a container", bulletPoints: [
+    "Docker container: running image + runtime user data",
+    "Containers are considered short running and easily re-creatable",
+    "Don’t put important user data in containers",
+    "    Copy them as resources",
+    "    Load them as data volumes",
+    "See: Best practices for writing Dockerfiles",
+  ]),
 
-	// Section 3
+  Cover(title: "Example: drawRect.swift", bulletPoints: []),
 
-	Page(title: "Share the image on Docker Hub", bulletPoints: [
-		"Upload Docker images",
-   	"Automated Build",
-   	"    Link with a GitHub repo",
-   	"    Build images automatically (when pushed to repo)",
-   	"$ docker pull namespace/image_name",
-	]),
+  // Section 3
 
-	// Section 4
+  Page(title: "Share the image on Docker Hub", bulletPoints: [
+    "Upload Docker images",
+    "Automated Build",
+    "    Link with a GitHub repo",
+    "    Build images automatically (when pushed to repo)",
+    "$ docker pull namespace/image_name",
+  ]),
 
-	Page(title: "Swift.org on Docker", bulletPoints: [
-		"Only dependencies are built into the image",
-   	"All related projects are loaded into the container as a data volume",
-   	"Build and test in containers",
-	]),
+  // Section 4
 
-	Page(title: "swift-corelibs-foundation", bulletPoints: [
-		"-v <path to projects>:<path in container>",
-   	"The container should have at least 6GB of RAM",
-   	"Default RAM is 2GB",
-   	"    clang: error: unable to execute command: Killed",
-	]),
+  Page(title: "Swift.org on Docker", bulletPoints: [
+    "Only dependencies are built into the image",
+    "All related projects are loaded into the container as a data volume",
+    "Build and test in containers",
+  ]),
 
-	Cover(title: "Example: eyeplum/swift-foundation-dev", bulletPoints: []),
+  Page(title: "swift-corelibs-foundation", bulletPoints: [
+    "-v <path to projects>:<path in container>",
+    "The container should have at least 6GB of RAM",
+    "Default RAM is 2GB",
+    "    clang: error: unable to execute command: Killed",
+  ]),
 
-	// Ending
+  Cover(title: "Example: eyeplum/swift-foundation-dev", bulletPoints: []),
 
-	Cover(title: "Thank you!", bulletPoints: ["@eyeplum"]),
+  // Ending
 
-	Cover(title: "One more thing...", bulletPoints: []),
+  Cover(title: "Thank you!", bulletPoints: ["@eyeplum"]),
+
+  Cover(title: "One more thing...", bulletPoints: []),
 
 ])
